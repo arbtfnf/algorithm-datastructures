628e63708255ddb1dac87ec3a9b88e6241a33464
anurag rakesh bandhu <arbtfnf@users.noreply.github.com>
2019-09-17 01:51:23 +0530

    Create BFS

---
+++ graph/bfs.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, e, x, y;
+int vector<int> store[1000];
+int visited[1000];
+queue<int> q;
+
+void bfs(int k){
+    visited[k] = 1;
+    for(auto it = store[k].begin(); it != store.end(); it++){
+        if(!*it){
+            q.push(*it);
+            cout << *it << endl;
+            visited[*it] = 1;
+        }
+        int k = q.front();
+        q.pop();
+        bfs(k); 
+    }
+}
+
+int main(){
+    cin >> n >> e;
+
+    for(int i=1; i<=e; i++){
+        cin >> x >> y;
+        store[x].push_back(y);
+    }
+
+    for(int i=1; i<=n; i++){
+        if(!visited[i]) bfs(i);    
+    }
+    return 0;
+}
